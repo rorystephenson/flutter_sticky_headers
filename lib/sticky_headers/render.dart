4c6545d59d3440ee5ac54dd6bdff53a5bdf7b0c4
Rory Stephenson <rorystephenson@gmail.com>
2020-05-15 12:39:57 +0200

    Missed import

--- lib/sticky_headers/render.dart
+++ lib/sticky_headers/render.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a the MIT license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
